36294fb709133c3e8f00739b157192a84a50d5ab
PFalkowski <piotr.falkowski.fm@gmail.com>
2016-03-23 00:15:23 +0100

    emgu integrated

--- DetectContour/ViewModel.cs
+++ DetectContour/ViewModel.cs
@@ -9,13 +9,22 @@ using System.Threading.Tasks;
 using System.Windows.Controls;
 using SpatialMaps;
 using System.Windows.Media.Imaging;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using System.Drawing;
+using System.IO;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+using System.Windows;
 
 namespace DetectContour
 {
     public class ViewModel : BindableBase
     {
         public IOService InputOutputService = new DesktopIOService();
-        public const string PngFilter = "PNG|*.png";
+        public const string PngFilter = "Image|*.bmp;*.png;*.jpg;*.jpeg";
+        Image<Bgr, byte> frame;
+
         private BitmapImage _currentImage;
         public BitmapImage CurrentImage
         {
@@ -32,6 +41,22 @@ namespace DetectContour
                 }
             }
         }
+        private BitmapImage _contoursImage;
+        public BitmapImage ContoursImage
+        {
+            get
+            {
+                return _contoursImage;
+            }
+            set
+            {
+                if (value != _contoursImage)
+                {
+                    _contoursImage = value;
+                    OnPropertyChanged(nameof(ContoursImage));
+                }
+            }
+        }
         public ViewModel()
         {
             OpenImageCommand = new DelegateCommand(openImage);
@@ -45,12 +70,34 @@ namespace DetectContour
 
         private void openImage()
         {
-            var fileName = InputOutputService.GetFileNameForRead(null, null, PngFilter);
-            if (string.IsNullOrEmpty(fileName)) return;
-            CurrentImage = new BitmapImage(new Uri(fileName));
+            try
+            {
+                var fileName = InputOutputService.GetFileNameForRead(null, null, null);
+                if (string.IsNullOrEmpty(fileName)) return;
+                CurrentImage = new BitmapImage(new Uri(fileName));
+                ReadToBitmap(fileName);
+                GetContours();
+            }
+            catch (Exception ex)
+            {
+                InputOutputService.PrintToScreen(ex.Message, MessageSeverity.Error);
+            }
+        }
+
+        private void ReadToBitmap(string fileName)
+        {
+            frame = new Image<Bgr, byte>((Bitmap)Bitmap.FromFile(fileName));
+            _currentBitmap = (Bitmap)Bitmap.FromFile(fileName);
         }
 
         public DelegateCommand OpenImageCommand { get; private set; }
         public DelegateCommand SaveContoursCommand { get; private set; }
+
+        public const string outputFileName = "output.png";
+        private System.Drawing.Bitmap _currentBitmap = null;
+
+        private void GetContours()
+        {
+        }
     }
 }
