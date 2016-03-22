65a9bfc97fdb430549ad51e84c797b928c72665d
PFalkowski <piotr.falkowski.fm@gmail.com>
2016-03-22 16:36:35 +0100

    Now bitmaps load properly

--- DetectContour/ViewModel.cs
+++ DetectContour/ViewModel.cs
@@ -1,15 +1,37 @@
-﻿using Prism.Commands;
+﻿
+using Prism.Commands;
 using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
+using SpatialMaps;
+using System.Windows.Media.Imaging;
 
 namespace DetectContour
 {
     public class ViewModel : BindableBase
     {
+        public IOService InputOutputService = new DesktopIOService();
+        public const string PngFilter = "PNG|*.png";
+        private BitmapImage _currentImage;
+        public BitmapImage CurrentImage
+        {
+            get
+            {
+                return _currentImage;
+            }
+            set
+            {
+                if (value != _currentImage)
+                {
+                    _currentImage = value;
+                    OnPropertyChanged(nameof(CurrentImage));
+                }
+            }
+        }
         public ViewModel()
         {
             OpenImageCommand = new DelegateCommand(openImage);
@@ -23,7 +45,9 @@ namespace DetectContour
 
         private void openImage()
         {
-            throw new NotImplementedException();
+            var fileName = InputOutputService.GetFileNameForRead(null, null, PngFilter);
+            if (string.IsNullOrEmpty(fileName)) return;
+            CurrentImage = new BitmapImage(new Uri(fileName));
         }
 
         public DelegateCommand OpenImageCommand { get; private set; }
